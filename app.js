665d2a0de72dc424491e62880031536630a98b5a
Siôn Le Roux <sinisterstuf@gmail.com>
2015-09-23 23:37:40 +0200

    Add some logic to print out unset environment variables
    The Trello calls won't be able to work unless they can get the key,
    token and board from environment variables.  In case they haven't been
    set, the page will say so, so it's easier to figure out why it's not
    working.

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ var app = express()
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
+
+    var trello; // will contain auth stuff
+    var reqs = [
+        'TRELLO_KEY',
+        'TRELLO_TOKEN',
+        'TRELLO_BOARD'
+    ]
+
+    for(r in reqs) {
+        if (process.env[reqs[r]]) {
+            // save it into `trello`
+        } else {
+            // return an error
+            res.status(500).send(reqs[r] + ' not set')
+            return
+        }
+    }
+
     res.send('REcardo')
+
 })
 
 app.listen(app.get('port'), function() {
